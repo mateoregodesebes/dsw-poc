9bb725e4085be5942305d2e3110bc89df441f177
mateoregodesebes <mateoregodesebes123@gmail.com>
2024-09-12 11:46:55 -0300

    Basic chat schema

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
 import express from 'express'
 import logger from 'morgan'
+import WebSocket, { WebSocketServer } from 'ws'
+import { createServer } from 'node:http'
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
+
+const server = createServer(app)
+const wss = new WebSocketServer({ server })
+
+wss.on('connection', function connection(ws) {
+    console.log('Usuario conectado')
+
+    ws.on('message', function incoming(data) {
+        ws.send(data.toString())
+    })
+
+    ws.on('close', function close() {
+        console.log('Usuario desconectado')
+    })
+})
 
 app.use(logger('dev'))
 app.use(express.static(process.cwd() + '/client'))
@@ -12,6 +29,6 @@ app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
