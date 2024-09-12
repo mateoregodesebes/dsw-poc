5e73f015aaeb66fcda3072751a7e01805ce2707a
mateoregodesebes <mateoregodesebes123@gmail.com>
2024-09-12 12:27:54 -0300

    Small fix for intercommunication

--- server/index.js
+++ server/index.js
@@ -9,16 +9,21 @@ const app = express()
 
 const server = createServer(app)
 const wss = new WebSocketServer({ server })
+const clients = []
 
 wss.on('connection', function connection(ws) {
+    clients.push(ws)
     console.log('Usuario conectado')
 
     ws.on('message', function incoming(data) {
-        ws.send(data.toString())
+        for (let client of clients) {
+            client.send(data)
+        }
     })
 
     ws.on('close', function close() {
-        console.log('Usuario desconectado')
+        const socketIndex = clients.indexOf(ws)
+        clients.splice(socketIndex, 1)
     })
 })
 
